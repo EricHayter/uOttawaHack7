1c4a4acb1b9a3cc2fa342408cf56ee5dd3ea2bb1
mpalcz <megg.palcz@gmail.com>
2025-01-18 16:42:05 -0500

    GroqFO!!!!!!!!

--- backend/server.py
+++ backend/server.py
@@ -1,1 +1,26 @@
 from flask import Flask
+from markupsafe import escape
+import requests
+
+app = Flask(__name__)
+connected_users = []
+
+@app.route("/connect/<userid>/<device_addr>")
+def connect_user(userid, device_addr):
+    connected_users.append((userid,device_addr))
+    return f'User {escape(userid)} connected'
+
+@app.route("/disconnect/<userid>")
+def disconnect_user(userid):
+    connected_users = [(user, device) for (user, device) in connected_users if user != userid]
+    return f'User {escape(userid)} disconnected'
+
+@app.route("/<userid>")
+def get_user(userid):
+    devices = [device for (user, device) in connected_users if user == userid]
+    if not devices:
+        return '404'
+    for device in devices:
+        info = requests.get(device)
+        # groq_fo = call_groq(info)
+        return info.text

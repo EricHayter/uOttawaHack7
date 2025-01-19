d44f507f3c38d8a6930bcaaa2ec13dc758f02421
choiIsabelle <isabellelissina@gmail.com>
2025-01-19 09:50:27 -0500

    update results UI

--- frontend/src/LandingPageInputComponent.jsx
+++ frontend/src/LandingPageInputComponent.jsx
@@ -11,7 +11,7 @@ const StyledP = styled.p`
     margin-top: 0.25rem;
 `;
 
-const StyledTitle = styled.h1`
+export const StyledTitle = styled.h1`
     width: 500px;
     font-size: 30px;
     margin-top: 2rem;
@@ -51,56 +51,55 @@ const SubtitleContainer = styled.div`
     flex-direction: column;
 `;
 
+const GetUserUrl = ({url, setUrl, validateUrl}) => {
+    return (
+        <div>
+            <TitleCarousel 
+                texts={["is that Bobic? 🐕", "What's happening at home? 🏠", "Meow?! 😺"]} 
+                speed={150} 
+                pause={2000}
+            />
+            <InputContainer>
+                <StyledTitle>Where do you want to look? ✨</StyledTitle>
+                <SubtitleContainer>
+                    <StyledInputContainer>
+                        <StyledInput 
+                            value={url} 
+                            onChange={(e) => setUrl(e.target.value)}
+                            onClick={() => setUrl(" ")}
+                        />
+                        <StyledButton
+                            onClick={validateUrl}
+                        >
+                            Submit
+                        </StyledButton>
+                    </StyledInputContainer>
+                    <StyledP>💡We&apos;ll use this URL to connect to the remote camera you want to analyze.</StyledP>
+                </SubtitleContainer>
+            </InputContainer>
+        </div>
+    );
+};
+
+
 export const LandingPageInputComponent = () => { 
     const [didSubmit, setDidSubmit] = React.useState(false);
     const [showResults, setShowResults] = React.useState(false);
-
-    const GetUserUrl = () => {
-        const [url, setUrl] = React.useState("Enter the URL of the camera you would like to analyze.");
-
-        const validateUrl = () => {
-            console.log("Validating URL");
-            setDidSubmit(true);
-            setTimeout(() => {
-                setDidSubmit(false);
-                setShowResults(true);
-            }, 1750);
-        };
-
-        return (
-            <div>
-                <TitleCarousel 
-                    texts={["is that Bobic? 🐕", "What's happening at home? 🏠", "Meow?! 😺"]} 
-                    speed={150} 
-                    pause={2000}
-                />
-                <InputContainer>
-                    <StyledTitle>Where do you want to look? ✨</StyledTitle>
-                    <SubtitleContainer>
-                        <StyledInputContainer>
-                            <StyledInput 
-                                value={url} 
-                                onChange={(e) => setUrl(e.target.value)}
-                                onClick={() => setUrl(" ")}
-                            />
-                            <StyledButton
-                                onClick={validateUrl}
-                            >
-                                Submit
-                            </StyledButton>
-                        </StyledInputContainer>
-                        <StyledP>💡We&apos;ll use this URL to connect to the remote camera you want to analyze.</StyledP>
-                    </SubtitleContainer>
-                </InputContainer>
-            </div>
-        );
+    const [url, setUrl] = React.useState("Enter the URL of the camera you would like to analyze.");
+    
+    const validateUrl = () => {
+        console.log("Validating URL");
+        setDidSubmit(true);
+        setTimeout(() => {
+            setDidSubmit(false);
+            setShowResults(true);
+        }, 1750);
     };
-
     return (
         <>
             {didSubmit && <LandingPageBuffer />}
-            {!didSubmit && !showResults && <GetUserUrl />}
-            {showResults && <ResultsPage />}
+            {!didSubmit && !showResults && <GetUserUrl url={url} setUrl={setUrl} validateUrl={validateUrl} />}
+            {showResults && <ResultsPage url={url} />}
         </>
     );
 };
--- frontend/src/ResultsPage.jsx
+++ frontend/src/ResultsPage.jsx
@@ -1,5 +1,63 @@
-export const ResultsPage=()=>{
+import styled from "styled-components"
+import cat from "/cat.mp4?url"
+
+const StyledContainer = styled.div`
+    display: flex;
+    flex-direction:column;
+    gap: 0.5rem;
+`
+
+const ButtonContainer = styled.div`
+    display: flex;
+    margin-top: 0.5rem;
+    justify-content: center;
+    gap: 2rem;
+    width: 100%;
+`
+
+const StyledOption1 = styled.button`
+    background-color:rgba(85, 87, 238, 0.86);
+    color: white;
+    width: 40%;
+`
+const StyledOption2 = styled.button`
+    width: 40%;
+        color: white;
+    background-color:rgba(216, 109, 8, 0.75);
+`
+
+const StyledTitle = styled.h1`
+    font-size: 30px;
+    font-weight: 500;
+    margin-top: 2rem;
+`;
+
+const StyledText = styled.p`
+    font-size: 15px;
+    padding: 0.4rem;
+    font-weight: 500;
+`;
+
+const VideoContainer = styled.div`
+    margin-bottom: 0.5rem;
+`
+
+
+export const ResultsPage=({url})=>{
+    console.log(url)
     return(
-        <div>Live video stream here!</div>
+        <StyledContainer>
+        <VideoContainer>
+            <StyledTitle>Located Video!</StyledTitle>
+            <StyledText>{`The video found at ${url} is shown below! Use the buttons to navigate more options.`}</StyledText>
+        </VideoContainer>
+            <video controls>
+                <source src={cat} type="video/mp4" width="600"/>
+            </video>
+        <ButtonContainer>
+            <StyledOption1>Look for suspicious activities 📢</StyledOption1>
+            <StyledOption2>Investigate a new video 🔎</StyledOption2>
+        </ButtonContainer>
+        </StyledContainer>
     )
 }
